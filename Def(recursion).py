b1f3ecce0b6f6b16e2e4aff364d6a655c92401c2
Froggit2 <165292357+Froggit2@users.noreply.github.com>
2024-04-26 12:48:55 +0300

    Delete Def(recursion).py

--- Def(recursion).py
+++
@@ -1,9 +0,0 @@
-import math                         #в интернете нашел я сам не знал как сделать
-def test(a = 1, b = 'два', c = True,n = 0):
-    print(a,b,c)
-    if n == 0:
-        return 0
-    else:
-        n = math.factorial(n)       #в интернете нашел я сам не знал как сделать
-    print(n)
-test(n = 5)
